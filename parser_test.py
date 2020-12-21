aa36ec935d5aa66b1abaafe6a72bf1542e228aad
ryndovaira <ryndovaira@gmail.com>
2020-12-21 17:48:45 +0300

    Update

--- parser_test.py
+++ parser_test.py
@@ -45,7 +45,7 @@ def test_parse_string():
     }
 
 
-def test_parse_age_gt_25_or_exp_ne_05():
+def test_parse_or():
     assert parse('Возраст>25 OR Стаж!=0.5') == {
         "op": "OR",
         "type": "node",
@@ -64,7 +64,7 @@ def test_parse_age_gt_25_or_exp_ne_05():
     }
 
 
-def test_parse_age_gt_25_and_exp_ne_05():
+def test_parse_and():
     assert parse('Возраст>25 AND Стаж!=0.5') == {
         "op": "AND",
         "type": "node",
@@ -83,36 +83,7 @@ def test_parse_age_gt_25_and_exp_ne_05():
     }
 
 
-def test_parse_age_gt_25_and_exp_ne_05_or_age_le_60():
-    assert parse('Возраст>25 AND Стаж!=0.5 OR Возраст<=60') == {
-        "op": "OR",
-        "type": "node",
-        "left": {
-            "op": "AND",
-            "type": "node",
-            "left": {
-                "op": ">",
-                "id": "Возраст",
-                "literal": 25,
-                "type": "leaf"
-            },
-            "right": {
-                "op": "!=",
-                "id": "Стаж",
-                "literal": 0.5,
-                "type": "leaf"
-            }
-        },
-        "right": {
-            "op": "<=",
-            "id": "Возраст",
-            "literal": 60,
-            "type": "leaf"
-        }
-    }
-
-
-def test_example_without_brackets_3():
+def test_parse_and_or():
     assert parse('Пол="М" AND Возраст>25 OR Стаж>.5') == {
         "op": "OR",
         "type": "node",
@@ -141,7 +112,7 @@ def test_example_without_brackets_3():
     }
 
 
-def test_example_without_brackets_5():
+def test_parse_and_or_and_or():
     assert parse('Пол="М" AND Возраст>25 OR Стаж>.5 AND Рост=1.86 OR Имя="Вася"') == {
         "op": "OR",
         "type": "node",
@@ -180,7 +151,7 @@ def test_example_without_brackets_5():
     }
 
 
-def test_example_with_brackets_0():
+def test_example_and_or_brackets_0():
     assert parse('Пол="М" AND (Возраст>25 OR Стаж>.5)') == {
         'type': 'node', 'op': 'AND',
         'left': {'type': 'leaf', 'op': '=', 'id': 'Пол', 'literal': "М"},
@@ -192,7 +163,7 @@ def test_example_with_brackets_0():
     }
 
 
-def test_example_with_brackets_1():
+def test_example_and_or_brackets_1():
     assert parse('(Пол="М" AND Рост=1.86) AND (Возраст>25 OR Стаж>.5)') == {
         "op": "AND",
         "type": "node",
@@ -231,36 +202,7 @@ def test_example_with_brackets_1():
     }
 
 
-def test_example_with_brackets_2():
-    assert parse('(Пол="М" AND Рост=1.86 AND Возраст>25 OR Стаж>.5)') == {
-        "op": "OR",
-        "type": "node",
-        "left": {
-            "op": "AND",
-            "type": "node",
-            "left": {
-                "op": "=",
-                "id": "Пол",
-                "literal": "М",
-                "type": "leaf"
-            },
-            "right": {
-                "op": "=",
-                "id": "Рост",
-                "literal": 1.86,
-                "type": "leaf"
-            }
-        },
-        "right": {
-            "op": ">",
-            "id": "Стаж",
-            "literal": 0.5,
-            "type": "leaf"
-        }
-    }
-
-
-def test_example_with_brackets_3():
+def test_example_and_or_brackets_2():
     assert parse('((((((Пол="М" AND Рост=1.86))) AND Возраст>25 OR Стаж>.5)))') == {
         "op": "OR",
         "type": "node",
@@ -299,7 +241,7 @@ def test_example_with_brackets_3():
     }
 
 
-def test_example_with_brackets_4():
+def test_example_and_or_brackets_3():
     assert parse('((Пол="М" AND ((Рост=1.86) AND (Возраст>25))) OR Стаж>.5)') == {
         "op": "OR",
         "type": "node",
